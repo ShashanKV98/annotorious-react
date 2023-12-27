eece4fba8daa77cce32a6164dcf6119129cc1f71
ShashanKV98 <vshashank32@gmail.com>
2023-12-27 04:50:57 -0700

    Delete dist directory

--- dist/openseadragon/annotorious-react-osd.es5.js
+++
@@ -1,9 +0,0 @@
-import { __module as e } from "./annotorious-react-osd.es10.js";
-import { __require as r } from "./annotorious-react-osd.es11.js";
-import { __require as o } from "./annotorious-react-osd.es12.js";
-process.env.NODE_ENV === "production" ? e.exports = o() : e.exports = r();
-var m = e.exports;
-export {
-  m as j
-};
-//# sourceMappingURL=annotorious-react-osd.es5.js.map
--- dist/openseadragon/setPosition.d.ts
+++
@@ -1,3 +0,0 @@
-import { ImageAnnotation } from '@annotorious/annotorious';
-export declare const setPosition: (viewer: OpenSeadragon.Viewer, annotation: ImageAnnotation, popup: HTMLElement, sideOffset?: number) => void;
-//# sourceMappingURL=setPosition.d.ts.map
